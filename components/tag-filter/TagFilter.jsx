5453973cbed5a202d28682598bb8f07ffc269f6f
bmenc <benjamin.menchaca@imedmex.com.mx>
2025-05-20 10:27:51 -0600

    Replace initial layout with a centered TagFilter component

---
+++ components/tag-filter/TagFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TagFilter = () => {
+    return (
+        <div>
+            {/* Contenido del componente TagFilter */}
+            <p>TagFilter123</p>
+        </div>
+    );
+};
+
+export default TagFilter;
